06d822693cd339c3d23cfe59cb8df983143a163b
ayodyabandara <ayodyabandara0@gmail.com>
2026-02-20 16:12:52 +0530

    Add Web Server example for Lab 04

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-console.log("Hello from Lab 04!");
-const fs = require('fs');
-
-// Write to file
-fs.writeFile('file.txt', 'Hello from Lab 04!', (err) => {
-  if (err) throw err;
-  console.log('File saved!');
-});
-
-// Read from file
-fs.readFile('file.txt', 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log('File content:', data);
-});
-//create a web server
 const http = require('http');
+
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!');
   res.end();
 }).listen(8080);
 
+console.log("Server running at http://localhost:8080/");
