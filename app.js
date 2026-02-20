6dfd134e1db7a0e7f2e0fb1340a418cea3adcef1
ayodyabandara <ayodyabandara0@gmail.com>
2026-02-20 16:07:30 +0530

    Add app.js with console log, file write and read for Lab 04

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ fs.readFile('file.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log('File content:', data);
 });
+//create a web server
+const http = require('http');
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
